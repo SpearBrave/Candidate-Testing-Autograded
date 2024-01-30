691392aa74ef12394ee0ed90a3d1596287f4ad86
SpearBrave <51465580+SpearBrave@users.noreply.github.com>
2024-01-29 20:41:21 -0600

    Minor change

--- candidate-testing.js
+++ candidate-testing.js
@@ -62,6 +62,7 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+    
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
