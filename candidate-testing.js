cc4d07a68941934deb3cd440e233374872365c90
SpearBrave <gerardovazquez9509@gmail.com>
2024-02-03 14:36:30 -0600

    FINAL

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,58 +1,77 @@
 const input = require('readline-sync');
 
+
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
+
+// TODO 1.1a: Define candidateName //
 let candidateName= "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question="Who was the first American woman in space? ";
 let correctAnswer="Sally Ride";
 let candidateAnswer="";
 
+
 //TODO: Variables for Part 2
 let questions=["Who was the first American woman in space? " ,"True or false: 5 kilometer == 5000 meters? " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? "   , "What is the minimum crew size for the ISS? " ];
 let correctAnswers= ['Sally Ride','true'  ,'40'  , 'Trajectory'   ,  '3'   ];
 
+
 let candidateAnswers=[];
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName= input.question("Name?");
 }
 
+
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-for (let i = 0; i < questions.length ; i++)                                                                           
+
+for (let i = 0; i < questions.length ; i++)                                                                          
 {let groupA = input.question(`${questions[i]}`);                                      
 candidateAnswers.push(groupA);
 }}
 
+
 //Now need to turn string into Array
+
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
-
-
-
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
 let correct= 0;
 
+
 for (let i = 0; i < 5; i++){
+
+
+  console.log(`${questions[i]}`+ "\n Candidate Answer:"+`${candidateAnswers[i]}`+"\n Correct Answer :"+`${correctAnswers[i]}`);
   if (candidateAnswers[i].toLowerCase()===correctAnswers[i].toLowerCase()){
       correct = correct + 1 ;
 }
-  
+ 
 }
 let grade =0 ;  //TODO 3.2 use this variable to calculate the candidates score.
-  grade = (correct) / (questions.length) * 100; 
-console.log(grade);
-console.log("Answers",candidateAnswers,"\n","Correct Responses",correctAnswers)  ;
+  grade = (correct) / (questions.length) * 100;
+
+if (grade > 80 ){
+  console.log("Grade:",grade+" Pass");
+
+
+}else {
+  console.log("Grade:",grade+" Fail");
+}
+//console.log(candidateName,"\n","Answers",candidateAnswers,"\n","Correct Responses",correctAnswers)  ;
+
 
 return (grade);
 }
+
+
 
 
 function runProgram() {
@@ -62,7 +81,10 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
-    
+   
+
+
+
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
